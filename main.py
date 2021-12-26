7df38eac77188767595cb35d2cd3ed69c854fb3f
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2021-12-26 13:41:51 +0100

    Write code for win scenarios

--- main.py
+++ main.py
@@ -3,21 +3,46 @@ BOARD = f'''
 -----------
  4 | 5 | 6 
 -----------
- 7 | 8 | 9 '''
+ 7 | 8 | 9 
+'''
 
-BOARD_VALUES = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+BOARD_VALUES = [[" ", " ", " "],
+                [" ", " ", " "],
+                [" ", " ", " "]]
+
+
+def checkList(lst):
+    if lst.count(" ") == 0:
+        return len(set(lst)) == 1
+
 
 def check_win():
     for row in BOARD_VALUES:
-        if row[0][0] == row[1][0] and row[0][0] == row[2][0]:
-            pass
+        if checkList(row):
+            print(f"Winner is player {row[0]}")
+            return True
+
+    columns = list(map(list, zip(*BOARD_VALUES)))
+    for column in columns:
+        if checkList(column):
+            print(f"Winner is player {column[0]}")
+            print("Column")
+            return True
+
+    if BOARD_VALUES[1][1] == BOARD_VALUES[0][0] and BOARD_VALUES[1][1] == BOARD_VALUES[2][2] \
+            or BOARD_VALUES[1][1] == BOARD_VALUES[0][2] and BOARD_VALUES[1][1] == BOARD_VALUES[2][0]:
+        print(f"Winner is player {BOARD_VALUES[1][1]}")
+        print("Accross")
+        return True
+
 
 def run():
-    print(BOARD)
+    while True:
+        print(BOARD)
+        if check_win():
+            break
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     run()
-
-
